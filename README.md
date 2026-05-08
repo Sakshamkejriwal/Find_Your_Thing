1223d309488caf6ce1e3a8b6be05d6058ae2b0e3
Saksham kejriwal <sakshamkejriwal09@gmail.com>
2026-05-08 10:14:00 +0530

    Initial project setup with Vite and Tailwind

---
+++ README.md
@@ -0,0 +1,2 @@
+# Find_Your_Thing
+Lost something? FYT helps students find misplaced items through smart matching and realtime reporting.
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="p-10 text-center">
+        <h1 className="text-3xl font-bold">FindYourThing Base</h1>
+      </div>
+    </BrowserRouter>
+  );
+}
+export default App;
